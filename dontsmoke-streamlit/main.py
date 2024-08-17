872ef86b8930a6bd69118aa3916eecd3f53666e3
Codeeefactory <codeefactory@gmail.com>
2024-08-17 18:20:42 +0330

    docker needed

--- dontsmoke-streamlit/main.py
+++ dontsmoke-streamlit/main.py
@@ -3,5 +3,7 @@ import requests
 
 st.title("مصرف نکن")
 r=requests.get("http://127.0.0.1:8000/Contents")
-for ad in r.json()[1]:
-    st.write(ad)
+print(r.json())
+for ad in r.json():
+    for x,y in zip(ad.keys(),ad.values()) :
+        st.html(f"<h1>{y}</h1>")
